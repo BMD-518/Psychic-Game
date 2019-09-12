1930703b3e2cafe520fa0d9d05c957afa54fe6d5
Bijan Dayani <bdayani518@gmail.com>
2019-09-12 13:26:00 -0400

    added loss if statement for resetting variables and logging loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,23 @@ var loss = 0;
 var guesses = 10;
 var guessArray = [];
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var compGuess = compGuess;
+
 
 // use Math.random to generate compGuess value and correlate value with alphabet array. Math.random generates a random number with a value between 0-1, multiplied by alphabet.length (26). Math.floor then rounds the number down to the next lowest whole number.
 
 compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 // console.log to confirm
-console.log(compGuess);
+console.log("compGuess",compGuess);
 
 // create function for resetting compGuess between rounds.
 function funGuess() {
     compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(compGuess);
+    console.log("compGuess",compGuess);
 }
 
-// document event.key for regognition of input from keyboard
+// listener for regognition of input from keyboard
 document.onkeypress = function(event) {
     var userGuess = event.key;
-
     // limit key selection to letters a-z and create alert if non-alphabetic key is pressed
     var limit = /[a-z]/gi;
     if (!limit.test(userGuess)) {
@@ -32,26 +31,33 @@ document.onkeypress = function(event) {
     }
     
     
-    
-    
-    
     // check for matching value between userGuess and compGuess
     if (compGuess === userGuess) {
         console.log('Win');
         alert('You must be reading my mail...')
         document.getElementById("win").innerHTML = win+=1;
-        userGuess = [];
-        document.getElementById("userGuess").innerHTML = userGuess;
+        guessArray = [];
+        document.getElementById("guessArray").innerHTML = userGuess;
         funGuess();
-        console.log(compGuess);
         guesses = 10;
         document.getElementById("guesses").innerHTML = guesses;
     }
     else {
-        console.log("Oh, come on!")
+        console.log("Wrong!");
         document.getElementById("guesses").innerHTML = guesses-=1;
         guessArray.push(userGuess);
         document.getElementById("guessArray").innerHTML = guessArray;
     }
-   
+    // detect loss, add to loss counter, reset guesses, run compGuess()
+    if (guesses <= 0) {
+        loss+=1;
+        document.getElementById("loss").innerHTML = loss+=1;
+        console.log("Loss")
+        alert("Ya blew it!")
+// *** guesses not returning to 10 after loss.
+        guesses = 10;
+// *** guessArray not resetting after loss.
+        guessArray = [];
+        funGuess();
+    }
 }
